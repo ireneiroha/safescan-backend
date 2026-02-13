8581566fe47a8ceef396836f480c0ae1e798e6b1
ireneiroha <ireneiroha@gmail.com>
2026-02-13 23:12:41 +0100

    Update backend with swagger version

--- backend/src/controllers/auth.controller.js
+++
@@ -1,8 +0,0 @@
-
-exports.register = async (req, res) => {
-  res.json({ message: 'User registered (mock)' });
-};
-
-exports.login = async (req, res) => {
-  res.json({ token: 'mock-jwt-token' });
-};
